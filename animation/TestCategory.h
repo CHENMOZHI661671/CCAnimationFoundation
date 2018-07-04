e443c57d1a5cde00f3c2ff6db96e939cf97031a7
chenmozhi <1025553907@qq.com>
2018-07-04 12:26:52 +0800

    modify spec file

---
+++ animation/TestCategory.h
@@ -0,0 +1,15 @@
+//
+//  TestCategory.h
+//  QXUtilityKit
+//
+//  Created by quixote on 16/8/25.
+//  Copyright © 2016年 quixote. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestCategory : NSObject
+
+- (void)test;
+
+@end
